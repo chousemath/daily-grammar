1aa182b390409a78d6ea228046bfb9084b7eab36
Joseph Sungpil Choi <chousemath@gmail.com>
2020-01-19 09:31:51 +0900

    Initial Commit

---
+++ GrammarHack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GrammarHack
+//
+//  Created by Joseph Sungpil Choi on 2020/01/19.
+//  Copyright © 2020 Paw Kitchen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
