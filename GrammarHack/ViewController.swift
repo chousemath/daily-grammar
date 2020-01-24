96665afa66c558b834e3f9ee09b4399efa44e0d3
Joseph Sungpil Choi <chousemath@gmail.com>
2020-01-25 06:40:07 +0900

    Update

--- GrammarHack/ViewController.swift
+++ GrammarHack/ViewController.swift
@@ -835,6 +835,586 @@ var questions: [Quest] = [
     Reading(
         answer: "I would like an check card",
         kor: "체크 카드를 만들고 싶어요."
+    ),
+    Reading(
+        answer: "I need to reset my password.",
+        kor: "비밀번호를 재발급 하고 싶어요."
+    ),
+    Reading(
+        answer: "Are there monthly fees?",
+        kor: "수수료 있어요?"
+    ),
+    Reading(
+        answer: "Do you need internet banking?",
+        kor: "인터넷 뱅킹 필요해요?"
+    ),
+    Reading(
+        answer: "I need to make a deposit.",
+        kor: "입금 해주세요."
+    ),
+    Reading(
+        answer: "I need to make a withdrawal.",
+        kor: "출금 해주세요."
+    ),
+    Reading(
+        answer: "How much is the wire transfer charge?",
+        kor: "송금 수수료는 얼마예요?"
+    ),
+    Reading(
+        answer: "Can I apply for a credit card?",
+        kor: "신용 카드 신청할 수 있어요?"
+    ),
+    Reading(
+        answer: "I need to see a doctor.",
+        kor: "진찰을 받고 싶어요."
+    ),
+    Reading(
+        answer: "I have health insurance.",
+        kor: "건강 보험 있어요."
+    ),
+    Reading(
+        answer: "I have a sore throat.",
+        kor: "목이 아파요."
+    ),
+    Reading(
+        answer: "I have indigestion",
+        kor: "소화불량 체했어요."
+    ),
+    Reading(
+        answer: "My nose is stuffy.",
+        kor: "코가 막혔어요."
+    ),
+    Reading(
+        answer: "My nose is runny.",
+        kor: "자꾸 콧물이 나요."
+    ),
+    Reading(
+        answer: "Where can I get this prescription filled?",
+        kor: "어디에서 약을 조제할 수 있어요?"
+    ),
+    Reading(
+        answer: "I have a prescription to fill.",
+        kor: "처방전을 받아 왔어요."
+    ),
+    Reading(
+        answer: "I have a cold.",
+        kor: "감기에 걸렸어요."
+    ),
+    Reading(
+        answer: "I don’t have any allergies.",
+        kor: "알레르기가 없어요."
+    ),
+    Reading(
+        answer: "I’m here to make an appointment.",
+        kor: "예약하려고 왔는데요."
+    ),
+    Reading(
+        answer: "How long do I have to wait?",
+        kor: "얼마나 기다려야해요?"
+    ),
+    Reading(
+        answer: "I am here for a cleaning.",
+        kor: "스케일링 하러 왔어요."
+    ),
+    Reading(
+        answer: "I'm here for a dental checkup.",
+        kor: "치과 검진 하러 왔어요."
+    ),
+    Reading(
+        answer: "My upper tooth hurts.",
+        kor: "윗니가 아파요."
+    ),
+    Reading(
+        answer: "My lower tooth is senstive.",
+        kor: "아랫니가 시려요."
+    ),
+    Reading(
+        answer: "My gums hurt.",
+        kor: "잇몸이 아파요."
+    ),
+    Reading(
+        answer: "I think I have a cavity.",
+        kor: "충치가 생긴 것 같아요."
+    ),
+    Reading(
+        answer: "I don’t have dental insurance.",
+        kor: "치과 보험 없어요."
+    ),
+    Reading(
+        answer: "I have dental insurance.",
+        kor: "치과 보험 있어요."
+    ),
+    Reading(
+        answer: "Please give me a haircut.",
+        kor: "커트 해주세요."
+    ),
+    Reading(
+        answer: "How much does a haircut cost?",
+        kor: "머리 커트 얼마예요?"
+    ),
+    Reading(
+        answer: "How long will it take?",
+        kor: "시간 얼마나 걸려요?"
+    ),
+    Reading(
+        answer: "Please just trim my hair.",
+        kor: "좀 다듬어 주세요."
+    ),
+    Reading(
+        answer: "Are there subtitles for this movie?",
+        kor: "영화 자막 있어요?"
+    ),
+    Reading(
+        answer: "What is the genre of the movie?",
+        kor: "영화는 무슨 장르에요?"
+    ),
+    Reading(
+        answer: "How long is the movie?",
+        kor: "영화 상영 시간이 얼마나 돼요?"
+    ),
+    Reading(
+        answer: "Where is the concession stand?",
+        kor: "매점은 어디 있어요?"
+    ),
+    Reading(
+        answer: "Do you give discounts to students?",
+        kor: "학생 할인 돼요?"
+    ),
+    Reading(
+        answer: "When is the last movie playing?",
+        kor: "마지막 영화는 언제예요?"
+    ),
+    Reading(
+        answer: "How much is the least expensive shipping option?",
+        kor: "제일 싼 우편 방법 얼마예요?"
+    ),
+    Reading(
+        answer: "Do I need a customs form?",
+        kor: "세관 서류 필요해요?"
+    ),
+    Reading(
+        answer: "Where can I buy stamps?",
+        kor: "우표는 어디에서 살 수 있어요?"
+    ),
+    Reading(
+        answer: "Do you sell envelopes?",
+        kor: "편지 봉투 팔아요?"
+    ),
+    Reading(
+        answer: "How long will it take to be delivered?",
+        kor: "배송기간은 얼마나 걸려요?"
+    ),
+    Reading(
+        answer: "What is the zip code for this address?",
+        kor: "이 주소의 우편번호가 뭐예요?"
+    ),
+    Reading(
+        answer: "Please give me some bubble wrap.",
+        kor: "뽁뽁이 좀 주세요."
+    ),
+    Reading(
+        answer: "I’m lost.",
+        kor: "길을 잃어버렸어요."
+    ),
+    Reading(
+        answer: "How far is it from here?",
+        kor: "여기에서 얼마나 걸리는지 아세요?"
+    ),
+    Reading(
+        answer: "Is it far from here?",
+        kor: "여기에서 멀어요?"
+    ),
+    Reading(
+        answer: "It’s quite far.",
+        kor: "꽤 멀어요."
+    ),
+    Reading(
+        answer: "It’s not far.",
+        kor: "멀지 않아요."
+    ),
+    Reading(
+        answer: "It’s close.",
+        kor: "가까워요."
+    ),
+    Reading(
+        answer: "It’s not within walking distance.",
+        kor: "걸어갈 만한 거리가 아니예요."
+    ),
+    Reading(
+        answer: "I’m sorry, I’m not from around here.",
+        kor: "미안하지만 저는 이 근처 사람이 아니예요."
+    ),
+    Reading(
+        answer: "Go left.",
+        kor: "왼쪽 가세요."
+    ),
+    Reading(
+        answer: "Go right.",
+        kor: "오른쪽에 가세요."
+    ),
+    Reading(
+        answer: "Go straight.",
+        kor: "쭉 가세요."
+    ),
+    Reading(
+        answer: "Do you have any rooms available?",
+        kor: "빈방 있어요?"
+    ),
+    Reading(
+        answer: "I would like to make a reservation.",
+        kor: "예약을 하고 싶어요."
+    ),
+    Reading(
+        answer: "Did you have a reservation?",
+        kor: "예약은 하셨습니까?"
+    ),
+    Reading(
+        answer: "I have a reservation.",
+        kor: "네, 예약했어요."
+    ),
+    Reading(
+        answer: "I don’t have a reservation.",
+        kor: "아직 예약을 하지 않았어요."
+    ),
+    Reading(
+        answer: "Is breakfast included with the room?",
+        kor: "요금에 조식은 포함되어 있어요?"
+    ),
+    Reading(
+        answer: "How long would you like to stay?",
+        kor: "몇 박을 하실 겁니까?"
+    ),
+    Reading(
+        answer: "May I see the room first?",
+        kor: "방을 먼저 봐도 돼요?"
+    ),
+    Reading(
+        answer: "May I have your name?",
+        kor: "성함을 말씀해주세요?"
+    ),
+    Reading(
+        answer: "I want to check out now.",
+        kor: "지금 체크 아웃하고 싶어요."
+    ),
+    Reading(
+        answer: "Please stop the bus here.",
+        kor: "여기서 세워 주세요."
+    ),
+    Reading(
+        answer: "What time is the last bus?",
+        kor: "마지막 버스 언제예요?"
+    ),
+    Reading(
+        answer: "Where can I charge my bus card?",
+        kor: "버스 카드 어디에 충전할 수 있어요?"
+    ),
+    Reading(
+        answer: "Where does this bus go?",
+        kor: "이 버스는 어디로 가요?"
+    ),
+    Reading(
+        answer: "It doesn’t taste good.",
+        kor: "맛없어요."
+    ),
+    Reading(
+        answer: "I’m hungry.",
+        kor: "배고파요."
+    ),
+    Reading(
+        answer: "I’m thirsty.",
+        kor: "목 말라요."
+    ),
+    Reading(
+        answer: "I’m a vegetarian.",
+        kor: "저는 채식주의자예요."
+    ),
+    Reading(
+        answer: "I don’t eat meat.",
+        kor: "저는 고기를 못 먹어요."
+    ),
+    Reading(
+        answer: "I can eat anything.",
+        kor: "다 먹을 수 있어요."
+    ),
+    Reading(
+        answer: "Nice to meet you.",
+        kor: "만나서 반가워요."
+    ),
+    Reading(
+        answer: "What is your name?",
+        kor: "이름이 뭐예요?"
+    ),
+    Reading(
+        answer: "Where are you from?",
+        kor: "어디에서 왔어요?"
+    ),
+    Reading(
+        answer: "How old are you?",
+        kor: "몇 살이에요?"
+    ),
+    Reading(
+        answer: "I can’t speak Korean well.",
+        kor: "한국말 잘 못해요."
+    ),
+    Reading(
+        answer: "I’m learning Korean these days.",
+        kor: "요즘 한국말 배워요."
+    ),
+    Reading(
+        answer: "What is your job?",
+        kor: "직업이 뭐예요?"
+    ),
+    Reading(
+        answer: "Where do you live?",
+        kor: "어디에 사세요?"
+    ),
+    Reading(
+        answer: "Are you married?",
+        kor: "결혼했어요?"
+    ),
+    Reading(
+        answer: "Long time no see.",
+        kor: "오랜만이에요."
+    ),
+    Reading(
+        answer: "Have you eaten?",
+        kor: "밥 먹었어요?"
+    ),
+    Reading(
+        answer: "How are you?",
+        kor: "잘 지내요?"
+    ),
+    Reading(
+        answer: "How have you been?",
+        kor: "잘 지냈어요?"
+    ),
+    Reading(
+        answer: "What have you been up to?",
+        kor: "어떻게 지냈어요?"
+    ),
+    Reading(
+        answer: "Where shall we go?",
+        kor: "어디로 갈까요?"
+    ),
+    Reading(
+        answer: "Get home safely.",
+        kor: "집에 잘 들어가요."
+    ),
+    Reading(
+        answer: "Just a moment please.",
+        kor: "잠시만 기다려 주세요."
+    ),
+    Reading(
+        answer: "Would you like to leave a message?",
+        kor: "메시지를 남기시겠어요?"
+    ),
+    Reading(
+        answer: "Can I take a message?",
+        kor: "메시지를 받을 수 있어요?"
+    ),
+    Reading(
+        answer: "Please repeat that for me.",
+        kor: "다시 말해 주세요."
+    ),
+    Reading(
+        answer: "Please speak up a little for me.",
+        kor: "좀 더 크게 말해 주세요."
+    ),
+    Reading(
+        answer: "I will call you back.",
+        kor: "제가 다시 걸게요."
+    ),
+    Reading(
+        answer: "When shall I call?",
+        kor: "언제쯤 통화가 가능할까요?"
+    ),
+    Reading(
+        answer: "Please charge my mobile phone for me.",
+        kor: "휴대폰 충전해 주세요."
+    ),
+    Reading(
+        answer: "Do you sell mobile phone cases?",
+        kor: "휴대폰 케이스 팔아요?"
+    ),
+    Reading(
+        answer: "Do you sell cell phone chargers?",
+        kor: "휴대폰 충전기 팔아요?"
+    ),
+    Reading(
+        answer: "Can I buy a data plan for my phone?",
+        kor: "휴대폰 데이터 플랜랜 살 수 있어요?"
+    ),
+    Reading(
+        answer: "Can you repair my mobile phone?",
+        kor: "휴대폰 고칠 수 있어요?"
+    ),
+    Reading(
+        answer: "How much will the repair cost?",
+        kor: "고치는 비용 얼마예요?"
+    ),
+    Reading(
+        answer: "When can I pick up my phone?",
+        kor: "휴대폰 언제 찾으러 올 수 있어요?"
+    ),
+    Reading(
+        answer: "Oh my goodness!",
+        kor: "아이고!"
+    ),
+    Reading(
+        answer: "Did a package arrive for me?",
+        kor: "소포 왔어요?"
+    ),
+    Reading(
+        answer: "I expect a package to arrive tomorrow.",
+        kor: "내일 소포가 올거예요."
+    ),
+    Reading(
+        answer: "My hot water doesn’t work.",
+        kor: "뜨거운 물이 안 나와요."
+    ),
+    Reading(
+        answer: "My hot water heater is broken.",
+        kor: "보일러가 고장났어요."
+    ),
+    Reading(
+        answer: "My sink is clogged.",
+        kor: "제 싱크대가 막혔어요."
+    ),
+    Reading(
+        answer: "My toilet is clogged.",
+        kor: "제 변기 막혔어요."
+    ),
+    Reading(
+        answer: "The lights don’t work.",
+        kor: "불이 안 켜져요."
+    ),
+    Reading(
+        answer: "The air conditioning isn’t cold.",
+        kor: "에어컨이 안 시원해요."
+    ),
+    Reading(
+        answer: "The neighbors are too loud.",
+        kor: "이웃이 너무 시끄러워요."
+    ),
+    Reading(
+        answer: "The neighbor's dog keeps barking.",
+        kor: "이웃집 강아지가 계속 짖어요."
+    ),
+    Reading(
+        answer: "I would like to find housing around here.",
+        kor: "이 근처의 집을 구하고 싶어요."
+    ),
+    Reading(
+        answer: "I would like to find a studio apartment.",
+        kor: "원룸 구하고 싶어요."
+    ),
+    Reading(
+        answer: "How much is the monthly rent?",
+        kor: "월세 얼마예요?"
+    ),
+    Reading(
+        answer: "How much is the deposit?",
+        kor: "보증금 얼마예요?"
+    ),
+    Reading(
+        answer: "How far is it from the subway station?",
+        kor: "지하철역에서 얼마나 걸려요?"
+    ),
+    Reading(
+        answer: "This is too small.",
+        kor: "너무 작아요."
+    ),
+    Reading(
+        answer: "This is too big.",
+        kor: "너무 커요."
+    ),
+    Reading(
+        answer: "This is too expensive.",
+        kor: "너무 비싸요."
+    ),
+    Reading(
+        answer: "I don’t really like it.",
+        kor: "별로 안 좋아요."
+    ),
+    Reading(
+        answer: "I like this house a lot.",
+        kor: "이 집 마음 들어요."
+    ),
+    Reading(
+        answer: "What is your phone number?",
+        kor: "핸드폰 번호가 뭐예요?"
+    ),
+    Reading(
+        answer: "Do you have time this weekend?",
+        kor: "주말에 시간 있어요?"
+    ),
+    Reading(
+        answer: "Shall we go on a date together?",
+        kor: "우리 데이트 할까요?"
+    ),
+    Reading(
+        answer: "Shall we get dinner together sometime?",
+        kor: "언제 우리 같이 저녁 먹을까요?"
+    ),
+    Reading(
+        answer: "You are pretty!",
+        kor: "예쁘네요!"
+    ),
+    Reading(
+        answer: "You are handsome!",
+        kor: "잘 생겼어요!"
+    ),
+    Reading(
+        answer: "You are charming!",
+        kor: "매력적이네요!"
+    ),
+    Reading(
+        answer: "When shall we meet again?",
+        kor: "언제 다시 볼까요?"
+    ),
+    Reading(
+        answer: "I will give you the address.",
+        kor: "주소를 알려 드릴게요."
+    ),
+    Reading(
+        answer: "This is an emergency.",
+        kor: "아주 급해요."
+    ),
+    Reading(
+        answer: "Where is the nearest police station?",
+        kor: "제일 가까운 경찰서는 어디예요?"
+    ),
+    Reading(
+        answer: "Where is the closest hospital?",
+        kor: "가장 가까운 병원은 어디예요?"
+    ),
+    Reading(
+        answer: "Please call an ambulance.",
+        kor: "구급차를 불러 주세요."
+    ),
+    Reading(
+        answer: "Please take me to the hospital.",
+        kor: "병원에 데려가 주세요."
+    ),
+    Reading(
+        answer: "Please help me.",
+        kor: "도와주세요."
+    ),
+    Reading(
+        answer: "Where can I get help?",
+        kor: "어디에서 도움을 구할 수 있어요?"
+    ),
+    Reading(
+        answer: "I am a diabetic.",
+        kor: "저는 당뇨가 있어요."
+    ),
+    Reading(
+        answer: "You can do it!",
+        kor: "잘할 수 있어요!"
+    ),
+    Reading(
+        answer: "Don’t give up.",
+        kor: "포기 하지마세요."
     )
 ]
 
