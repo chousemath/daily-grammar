714a3e94d82d3271b94296f6ab445b5257953e07
Joseph Sungpil Choi <chousemath@gmail.com>
2020-02-01 13:17:16 +0900

    Add announcement

--- GrammarHack/AppInfoViewController.swift
+++ GrammarHack/AppInfoViewController.swift
@@ -8,6 +8,22 @@
 
 import UIKit
 
+class Announcement: Codable {
+    let title: String
+    let body: String
+    let createdAt: String
+    
+    enum CodingKeys: String, CodingKey {
+        case title, body, createdAt
+    }
+    
+    init(title: String, body: String, createdAt: String) {
+        self.title = title
+        self.body = body
+        self.createdAt = createdAt
+    }
+}
+
 class AppInfoViewController: UIViewController {
     @IBOutlet var appVersionLabel: UILabel!
     @IBOutlet var announcementsLabel: UILabel!
